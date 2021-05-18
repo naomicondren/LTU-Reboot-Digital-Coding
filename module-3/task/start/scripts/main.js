fb50b7dc901f279f3c1a34c8e63ca5c1e2ddf27f
naomicondren <83037023+naomicondren@users.noreply.github.com>
2021-05-18 23:01:44 +0100

    added image container

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,22 @@
+console.log('Hello');
+
+// Single line comment 
+
+/* 
+
+Multiple line comment 
+
+*/ 
+
+var productName = 'Tinned Beans';
+let price = '0.40';
+const quantity = 1;
+var inStock = true;
+let discountAmount = 0;
+
+console.log(productName);
+console.log(price);
+console.log(quantity);
+console.log(inStock);
+console.log(discountAmount);
+
